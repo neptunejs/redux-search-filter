23dbd508e92114f0e68ecf3c0e8aee1c5814203d
Daniel Kostro <kostro.d@gmail.com>
2017-08-03 10:36:33 +0200

    feat: AND/OR and negate filter options
    For value and multiple filters (not range)

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -2,3 +2,5 @@ export const prefix = '@@redux-search-filter/';
 
 export const RESET = `${prefix}RESET`;
 export const UPDATE_FILTER = `${prefix}UPDATE_FILTER`;
+export const SET_OPERATOR = `${prefix}SET_OPERATOR`;
+export const SET_NEGATED = `${prefix}SET_NEGATED`;
--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,22 @@ import {Map} from 'immutable';
 
 import {
     RESET,
-    UPDATE_FILTER
+    UPDATE_FILTER,
+    SET_OPERATOR,
+    SET_NEGATED
 } from './actionTypes';
+
+const defaultMultipleFilter = {
+    value: [],
+    negated: false,
+    operator: 'AND'
+};
+
+const defaultValueFilter = {
+    value: [],
+    negated: false,
+    operator: 'OR'
+};
 
 export default function (state = new Map(), action) {
     let name, filter;
@@ -19,7 +33,23 @@ export default function (state = new Map(), action) {
             if (action.payload === null) {
                 return state.set(name, filter.delete(action.meta.prop));
             } else {
-                return state.set(name, filter.set(action.meta.prop, formatValue(action.meta.kind, action.payload)));
+                return state.set(name, filter.set(action.meta.prop, formatValue(action.meta.kind, filter.get(action.meta.prop), action.payload)));
+            }
+        }
+        case SET_NEGATED: {
+            const oldValue = filter.get(action.meta.prop);
+            if (oldValue && oldValue.inverted === action.payload) {
+                return state;
+            } else {
+                return state.set(name, filter.set(action.meta.prop, setNegated(action.meta.kind, oldValue, action.payload)));
+            }
+        }
+        case SET_OPERATOR: {
+            const oldValue = filter.get(action.meta.prop);
+            if (oldValue && oldValue.operator === action.payload) {
+                return state;
+            } else {
+                return state.set(name, filter.set(action.meta.prop, setOperator(action.meta.kind, oldValue, action.payload)));
             }
         }
         default:
@@ -27,13 +57,54 @@ export default function (state = new Map(), action) {
     }
 }
 
-function formatValue(kind, payload) {
+function setNegated(kind, filter, payload) {
+    switch (kind) {
+        case 'value':
+        case 'multiple': {
+            return Object.assign({}, getDefaultFilter(kind), filter, {
+                negated: payload
+            });
+        }
+        default: {
+            throw new Error(`unexpected kind: ${kind}`);
+        }
+    }
+}
+
+function setOperator(kind, filter, payload) {
+    switch (kind) {
+        case 'value':
+        case 'multiple': {
+            return Object.assign({}, getDefaultFilter(kind), filter, {
+                operator: payload
+            });
+        }
+        default: {
+            throw new Error(`unexpected kind: ${kind}`);
+        }
+    }
+}
+
+function formatValue(kind, filter, payload) {
     switch (kind) {
         case 'value':
         case 'multiple':
-            return Array.isArray(payload) ? payload : [payload];
+            return Object.assign({}, getDefaultFilter(kind), filter, {
+                value: Array.isArray(payload) ? payload : [payload]
+            });
         case 'range':
             return payload;
+        default:
+            throw new Error(`unexpected kind: ${kind}`);
+    }
+}
+
+function getDefaultFilter(kind) {
+    switch (kind) {
+        case 'multiple':
+            return defaultMultipleFilter;
+        case 'value':
+            return defaultValueFilter;
         default:
             throw new Error(`unexpected kind: ${kind}`);
     }
