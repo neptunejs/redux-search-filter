b3a770f9737c59c91d298875c29d8186efee8208
Michaël Zasso <targos@protonmail.com>
2017-08-07 13:36:56 +0200

    test: 100% coverage for filterData

--- src/__tests__/filter.js
+++ src/__tests__/filter.js
@@ -2,10 +2,10 @@ import {Map} from 'immutable';
 import filterData from '../filterData';
 
 const data = [
-    {A: ['Y'], B: 'Y'},
-    {A: ['Z'], B: 'Z'},
-    {A: ['Z', 'Y'], B: 'X'},
-    {A: ['X'], B: 'Y'},
+    {A: ['Y'], B: 'Y', C: 1},
+    {A: ['Z'], B: 'Z', C: 3},
+    {A: ['Z', 'Y'], B: 'X', C: 2},
+    {A: ['X'], B: 'Y', C: -1},
 ];
 data.forEach((d, idx) => d.idx = idx);
 
@@ -40,12 +40,29 @@ describe('Test filter', () => {
             {value: ['X', 'Y'], operator: 'OR', expected: [0, 2, 3]},
             {value: ['X'], expected: [2]},
             {value: ['X', 'Y'], operator: 'AND', negated: true, expected: [1]},
-            {value: ['X', 'Y'], operator: 'OR', negated: true, expected: [0, 1, 2, 3]}
+            {value: ['X', 'Y'], operator: 'OR', negated: true, expected: [0, 1, 2, 3]},
+            {value: ['Y'], negated: false, expected: [0, 3], operator: 'AND'}
         ];
 
         for (let filter of filters) {
             const f = Map({
                 B: filter
+            });
+            const filterIdx = filterData(data, f).map(v => v.idx).sort(asc);
+            expect(filterIdx).toEqual(filter.expected.sort(asc));
+        }
+    });
+
+    it('range', () => {
+        const filters = [
+            {min: 0, max: 3, expected: [0, 1, 2]},
+            {min: -2, max: 4, expected: [0, 1, 2, 3]},
+            {min: 0, max: 0.5, expected: []},
+            {min: 0.5, max: 1.5, expected: [0]}
+        ];
+        for (let filter of filters) {
+            const f = Map({
+                C: filter
             });
             const filterIdx = filterData(data, f).map(v => v.idx).sort(asc);
             expect(filterIdx).toEqual(filter.expected.sort(asc));
--- src/filterData.js
+++ src/filterData.js
@@ -1,4 +1,4 @@
-import {AND} from './constants/operators';
+import {AND, OR} from './constants/operators';
 
 export default function filterData(data, filters) {
     return data.filter((item) => {
@@ -7,8 +7,8 @@ export default function filterData(data, filters) {
             let operator = filterOptions.operator;
             let negated = filterOptions.negated;
             const itemValue = item[filterProp];
-            if (filterOptions.value) {
-                if (filterOptions.value.length) {
+            if (filterValue) {
+                if (filterValue.length > 0) {
                     if (Array.isArray(itemValue)) { // kind: multiple
                         operator = operator || AND;
                         if (operator === AND) {
@@ -37,33 +37,31 @@ export default function filterData(data, filters) {
                             }
                             return false;
                         }
-                    } else {
-                        if (Array.isArray(filterValue)) { // kind: value
-                            operator = operator || 'OR';
-                            const isIncluded = filterValue.includes(item[filterProp]);
-                            if (operator === AND) {
-                                if (negated) {
-                                    if (isIncluded) {
-                                        return false;
-                                    }
-                                } else {
-                                    if (filterValue.length > 1) return false;
-                                    if (!isIncluded) {
-                                        return false;
-                                    }
+                    } else { // kind: value
+                        operator = operator || OR;
+                        const isIncluded = filterValue.includes(item[filterProp]);
+                        if (operator === AND) {
+                            if (negated) {
+                                if (isIncluded) {
+                                    return false;
                                 }
                             } else {
-                                if (negated) {
-                                    if (filterValue.length > 1) {
-                                        continue;
-                                    }
-                                    if (isIncluded) {
-                                        return false;
-                                    }
-                                } else {
-                                    if (!isIncluded) {
-                                        return false;
-                                    }
+                                if (filterValue.length > 1) return false;
+                                if (!isIncluded) {
+                                    return false;
+                                }
+                            }
+                        } else {
+                            if (negated) {
+                                if (filterValue.length > 1) {
+                                    continue;
+                                }
+                                if (isIncluded) {
+                                    return false;
+                                }
+                            } else {
+                                if (!isIncluded) {
+                                    return false;
                                 }
                             }
                         }
